920905835e34cd54a8b17c8a9f6172c5102e4c89
Lucas Magalhães <lucoder618@gmail.com>
2025-07-20 15:05:05 -0300

    feat: baixando NF

--- src/whatsapp/clients/ClientWhatsappWebJs/ClientWhatsappWebJs.ts
+++ src/whatsapp/clients/ClientWhatsappWebJs/ClientWhatsappWebJs.ts
@@ -1,11 +1,41 @@
 import { Client, LocalAuth, MessageMedia } from 'whatsapp-web.js';
 import qrcode from 'qrcode-terminal';
-import { IMessageClient, ISendMessageConfigs, ISendMessageProps } from '../../interfaces/message-client';
+import { IMessageClient, ISendDocumentMessageProps, ISendMediaMessageProps, ISendMessageConfigs, ISendMessageProps } from '../../interfaces/message-client';
 import { IMessageData } from '../../interfaces/message-data';
 import { PersonNumber, PossibleClients } from '../../types/types';
 import AppError from '../../errors/AppError';
+import fetch from 'node-fetch';
 
 export default class ClientWhatsappWebJs implements IMessageClient {
+  async sendMediaMessage(props: ISendMediaMessageProps): Promise<any> {
+    const chatId = this._formatNumber(props.personNumber);
+    let media: MessageMedia;
+    if (props.mediaBuffer) {
+      media = new MessageMedia(props.mimeType || 'application/octet-stream', props.mediaBuffer.toString('base64'), props.caption || '');
+    } else if (props.mediaUrl) {
+      const response = await fetch(props.mediaUrl);
+      const buffer = await response.buffer();
+      media = new MessageMedia(props.mimeType || 'application/octet-stream', buffer.toString('base64'), props.caption || '');
+    } else {
+      throw new Error('mediaBuffer ou mediaUrl é obrigatório');
+    }
+    return this.client.sendMessage(chatId, media, { caption: props.caption });
+  }
+
+  async sendDocumentMessage(props: ISendDocumentMessageProps): Promise<any> {
+    const chatId = this._formatNumber(props.personNumber);
+    let media: MessageMedia;
+    if (props.documentBuffer) {
+      media = new MessageMedia(props.mimeType || 'application/pdf', props.documentBuffer.toString('base64'), props.fileName || 'document.pdf');
+    } else if (props.documentUrl) {
+      const response = await fetch(props.documentUrl);
+      const buffer = await response.buffer();
+      media = new MessageMedia(props.mimeType || 'application/pdf', buffer.toString('base64'), props.fileName || 'document.pdf');
+    } else {
+      throw new Error('documentBuffer ou documentUrl é obrigatório');
+    }
+    return this.client.sendMessage(chatId, media, { caption: props.caption });
+  }
   private client: Client;
   private eventCallbacks: { [event: string]: Function[] } = {};
 
--- src/whatsapp/interfaces/message-client.ts
+++ src/whatsapp/interfaces/message-client.ts
@@ -3,8 +3,29 @@ import { PossibleClients } from "../types/types";
 export interface IMessageClient {
   start(): Promise<void>;
   sendMessage(props: ISendMessageProps): Promise<any>;
+  sendMediaMessage(props: ISendMediaMessageProps): Promise<any>;
+  sendDocumentMessage(props: ISendDocumentMessageProps): Promise<any>;
   on(event: string, callback: Function): any;
   getClientType(): PossibleClients;
+}
+export interface ISendMediaMessageProps {
+  personNumber: string;
+  mediaType: 'image' | 'audio';
+  mediaUrl?: string;
+  mediaBuffer?: Buffer;
+  caption?: string;
+  mimeType?: string;
+  otherProps?: any;
+}
+
+export interface ISendDocumentMessageProps {
+  personNumber: string;
+  documentUrl?: string;
+  documentBuffer?: Buffer;
+  fileName?: string;
+  mimeType?: string; // ex: 'application/pdf'
+  caption?: string;
+  otherProps?: any;
 }
 
 export interface ISendMessageProps {
